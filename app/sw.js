0434b81f9e2effa3ce43ffb5c32b2f05b848517d
tomtak09281224 <tomtak09281224@gmail.com>
2026-08-08 11:45:07 +0900

    アイコン更新に合わせてService Workerのキャッシュバージョンを上げる

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meishi-app-cache-v1";
+const CACHE_NAME = "meishi-app-cache-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
