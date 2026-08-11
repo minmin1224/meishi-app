18387ebe3e6f60038c263bc200cecf2aa0058aa6
tomtak09281224 <tomtak09281224@gmail.com>
2026-08-12 08:34:42 +0900

    会社名判定を緩和(「会社」を含む行は法人格の接頭辞が無くても会社名と判定)

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meishi-app-cache-v3";
+const CACHE_NAME = "meishi-app-cache-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
